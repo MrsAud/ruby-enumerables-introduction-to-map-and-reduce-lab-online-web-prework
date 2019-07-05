f1b15c8c63e54c57ced2da31131e82715ea61784
Audrea Cook <audreacwork@gmail.com>
2019-07-05 16:04:11 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -57,15 +57,13 @@ def reduce_to_total(source_array, total = 0)
   total
 end
 
-def reduce_to_all_true(source_array, total = 0)
+def reduce_to_all_true(source_array)
   counter = 0
-  
-  if source_array[counter]
     while counter < source_array.length do
-      total += source_array[counter]
+      if source_array[counter]
       counter += 1
+      end
     end
-  end
   
   total
 end
