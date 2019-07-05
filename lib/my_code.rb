680a0a6ed9f6da1ce4bea62920e25ba61a144c42
Audrea Cook <audreacwork@gmail.com>
2019-07-05 15:51:28 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,3 +1,12 @@
 def map_to_negativize(source_array)
+  neg_array = []
+  counter = 0
   
+  if source_array[counter] < source_array.length do
+    neg_array << source_array[counter] * -1
+    
+    counter += 1
+  end
+  
+  neg_array
 end
