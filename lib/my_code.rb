f9fc26bd78b8fd13501b2a1e957c3ae57e54470a
Audrea Cook <audreacwork@gmail.com>
2019-07-05 16:00:45 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -45,3 +45,15 @@ def map_to_square(source_array)
   
   squared_array
 end
+
+def reduce_to_total(source_array)
+  sum = 0
+  counter = 0
+  
+  while counter < source_array.length do
+    sum += source_array[counter]
+    counter += 1
+  end
+  
+  sum
+end
