6d9301d109dbbdd01caf739f18062e33034b99a9
Audrea Cook <audreacwork@gmail.com>
2019-07-05 15:57:36 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -4,9 +4,20 @@ def map_to_negativize(source_array)
   
   while counter < source_array.length do
     neg_array << source_array[counter] * -1
-    
     counter += 1
   end
   
   neg_array
+end
+
+def map_to_no_change(source_array)
+  unchanged_array = []
+  counter = 0
+  
+  while counter < source_array.length do
+    unchanged_array << source_array[counter]
+    counter += 1
+  end
+  
+  unchanged_array
 end
