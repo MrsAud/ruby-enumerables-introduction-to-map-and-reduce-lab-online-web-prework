8bc25347180f47096ca6956361b7475e48c0ad07
Audrea Cook <audreacwork@gmail.com>
2019-07-05 16:01:31 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -50,9 +50,9 @@ def reduce_to_total(source_array, total = 0)
   counter = 0
   
   while counter < source_array.length do
-    sum += source_array[counter]
+    total += source_array[counter]
     counter += 1
   end
   
-  sum
+  total
 end
